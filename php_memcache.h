909692501eb3aeec5fbefb91b1a4920d30c6ab47
Mikael Johansson <mikl@php.net>
2008-09-11 20:00:11 +0000

    Prepare for release 3.0.2

--- php_memcache.h
+++ php_memcache.h
@@ -64,7 +64,7 @@ PHP_FUNCTION(memcache_decrement);
 PHP_FUNCTION(memcache_close);
 PHP_FUNCTION(memcache_flush);
 
-#define PHP_MEMCACHE_VERSION "3.0.2-dev"
+#define PHP_MEMCACHE_VERSION "3.0.2"
 
 #define MMC_DEFAULT_TIMEOUT 1				/* seconds */
 #define MMC_DEFAULT_RETRY 15 				/* retry failed server after x seconds */
