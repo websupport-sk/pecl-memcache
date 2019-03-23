9e26a14ed1166700dea3b6c82432f85b24ab9ad5
Tomas Srnka <tomas.srnka@gmail.com>
2019-03-23 08:17:33 +0100

    travis ci

---
+++ .travis/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo mkdir -p /var/run/memcached
+sudo chown memcache:memcache /var/run/memcached
+sudo echo "order hosts,bind" > /etc/host.conf
+sudo echo "multi off" >> /etc/host.conf
+
+phpize
+./configure
+make
+make install
+
+# Spawn memcached for tests
+/usr/bin/memcached -m 64 -u memcache -l -s /var/run/memcached/memcached.sock -d
+/usr/bin/memcached -m 64 -u memcache -U 11211 -l 127.0.0.1 -p 11211 -d
+/usr/bin/memcached -m 64 -u memcache -U 11212 -l 127.0.0.1 -p 11212 -d
+
+TEST_PHP_ARGS="--show-diff --keep-all -w fails.log" make test
