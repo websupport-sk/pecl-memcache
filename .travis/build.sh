a57cf75cd53d30a59bfff5142b33d5ed52a26317
Tomas Srnka <tomas.srnka@gmail.com>
2019-03-23 08:36:08 +0100

    Travis CI - memcached/sudo

--- .travis/build.sh
+++ .travis/build.sh
@@ -9,8 +9,8 @@ make
 make install
 
 # Spawn memcached for tests
-/usr/bin/memcached -m 64 -u memcache -l 127.0.0.1 -s /var/run/memcached/memcached.sock -d
-/usr/bin/memcached -m 64 -u memcache -U 11211 -l 127.0.0.1 -p 11211 -d
-/usr/bin/memcached -m 64 -u memcache -U 11212 -l 127.0.0.1 -p 11212 -d
+sudo /usr/bin/memcached -m 64 -u memcache -l -s /var/run/memcached/memcached.sock -d
+sudo /usr/bin/memcached -m 64 -u memcache -U 11211 -l 127.0.0.1 -p 11211 -d
+sudo /usr/bin/memcached -m 64 -u memcache -U 11212 -l 127.0.0.1 -p 11212 -d
 
 TEST_PHP_ARGS="--show-diff --keep-all -w fails.log" make test
