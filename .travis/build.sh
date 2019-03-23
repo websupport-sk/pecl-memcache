43ed4e62f88bf27f0051b55cd76e41d9fff023df
Tomas Srnka <tomas.srnka@gmail.com>
2019-03-23 08:29:48 +0100

    Socket memcached

--- .travis/build.sh
+++ .travis/build.sh
@@ -8,9 +8,8 @@ phpize
 make
 make install
 
-ls -la /var/run/memcached/
 # Spawn memcached for tests
-/usr/bin/memcached -m 64 -u memcache -l -s /var/run/memcached/memcached.sock -d
+/usr/bin/memcached -m 64 -u memcache -l 127.0.0.1 -s /var/run/memcached/memcached.sock -d
 /usr/bin/memcached -m 64 -u memcache -U 11211 -l 127.0.0.1 -p 11211 -d
 /usr/bin/memcached -m 64 -u memcache -U 11212 -l 127.0.0.1 -p 11212 -d
 
